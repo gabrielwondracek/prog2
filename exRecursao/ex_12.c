a35051d2aa2cdc6618ee5ec7bf00e1130269a148
gabrielwondracek <gabihw.2005@gmail.com>
2025-05-07 23:31:32 -0300

    exercícios de recursão terminados

--- exRecursao/ex_12.c
+++ exRecursao/ex_12.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
-#include <string.h>
 
-int main(){
+int palin(char vet[], int ini, int fim) {
+    if (ini >= fim) {
+        return 1; 
+    }
 
-    
+    if (vet[ini] != vet[fim]) {
+        return 0; 
+    }
+
+    return palin(vet, ini + 1, fim - 1);
+}
+
+int main() {
+    char vet[] = {'o', 's', 's', 'o'};
+    int tamanho = sizeof(vet) / sizeof(vet[0]);
+
+    if (palin(vet, 0, tamanho - 1)) {
+        printf("É palíndromo");
+    } else {
+        printf("Não é palíndromo");
+    }
+
     return 0;
 }
